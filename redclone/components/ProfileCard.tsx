610a6c063472f10df0a3cded756cd151c9bba3f6
JehadMR <106768742+JehadMR@users.noreply.github.com>
2022-07-07 22:59:59 +0300

    Update ProfileCard.tsx

--- redclone/components/ProfileCard.tsx
+++ redclone/components/ProfileCard.tsx
@@ -16,16 +16,27 @@ import Marquee from "react-fast-marquee";
 
 function ProfileCard () {
   const {data: session } = useSession();
-  const [player, setPlayer] = useState([]);
+  const [player, setPlayer] = useState({});
+  const [currentTrack, setCurrentTrack] = useState();
 
   const getPlayer = async () => {
-    const res = await axios.get('/api/currentplay');
-    const {items} = await res;
-    console.log(items);
-    setPlayer(items);
+   await axios.get(`/api/currentplay`)
+    .then(res => {
+      const items = res.data;
+      console.log(items);
+     setPlayer(items);
+    })
   };
 
- 
+
+  useEffect(() => {
+    let interval = setInterval(() => {
+     getPlayer();
+    }, player.item.duration_ms);
+    return () => {
+      clearInterval(interval);
+    };
+  }, {});
 
 
   return (
@@ -40,13 +51,20 @@ function ProfileCard () {
   */}
             <div className="min-w-0 py-5 pl-28 pr-5" >
             <div className="text-slate-900 font-medium text-sm sm:text-base truncate dark:text-slate-200">{session?.token?.name? session.token.name : <h1>"Sign In & Start Exploring!"</h1>}</div>
-            <div className="text-slate-500 font-medium text-sm sm:text-base leading-tight truncate dark:text-slate-400"><Marquee pauseOnHover={true} gradientWidth="50" >{"Heelo!"}</Marquee></div>
+            <div className="text-slate-500 font-medium text-sm sm:text-base leading-tight truncate dark:text-slate-400"><Marquee pauseOnHover={true} gradientWidth="50" >{player?.item?.name? player.item.name : "Song Here"}</Marquee></div>
             </div>
             </div>
-            <button onClick={() => console.log(player)}>CLICK ME</button>
+            
+
             </>
-      
+
+              
+                 
    )
   }
 export default ProfileCard
+
+function componentDidMount() {
+  throw new Error("Function not implemented.")
+}
 
