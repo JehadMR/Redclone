5a1a0f10e2b10fc923395fa41a8ce6d82ab8286f
JehadMR <106768742+JehadMR@users.noreply.github.com>
2022-07-11 00:35:37 +0300

    ProfileCard - Player timestamp

--- redclone/components/ProfileCard.tsx
+++ redclone/components/ProfileCard.tsx
@@ -35,9 +35,9 @@ function ProfileCard () {
     return beforeDecimalStr;
   }
   
-  const getProgress = () => {
-    let seconds = beforeDecimal(result?.data?.progress_ms? result.data.progress_ms / 1000 : 0);
-    let minutes = beforeDecimal(result?.data?.progress_ms? result.data.progress_ms / 60000 : 0 );
+  const getTimestamp = (ms_num) => {
+    let seconds = beforeDecimal(ms_num / 1000);
+    let minutes = beforeDecimal(ms_num / 60000);
 
     return (`${minutes}:${seconds[0]}${seconds[1]? seconds[1] : 0}`)
 
@@ -70,27 +70,27 @@ function ProfileCard () {
     </div>
     <div className="flex justify-between text-xs font-semibold text-gray-500 px-4 py-2">
       <div>
-          {getProgress()}
+          {getTimestamp(result?.data?.progress_ms? result.data.progress_ms : 0 )}
       </div>
       <div className="flex space-x-3 p-2">
         <button className="focus:outline-none">
-          <svg className="w-4 h-4" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="19 20 9 12 19 4 19 20"></polygon><line x1="5" y1="19" x2="5" y2="5"></line></svg>
+          <svg className="w-4 h-4" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="19 20 9 12 19 4 19 20"></polygon><line x1="5" y1="19" x2="5" y2="5"></line></svg>
         </button>
         <button className="rounded-full w-8 h-8 flex items-center justify-center pl-0.5 ring-2 ring-gray-100 focus:outline-none">
-          <svg className="w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="5 3 19 12 5 21 5 3"></polygon></svg>
+          <svg className="w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="5 3 19 12 5 21 5 3"></polygon></svg>
         </button>
         <button className="focus:outline-none">
-          <svg className="w-4 h-4" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="5 4 15 12 5 20 5 4"></polygon><line x1="19" y1="5" x2="19" y2="19"></line></svg>
+          <svg className="w-4 h-4" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="5 4 15 12 5 20 5 4"></polygon><line x1="19" y1="5" x2="19" y2="19"></line></svg>
         </button>
       </div>
       <div>
-        3:00
+        {getTimestamp(result?.data?.item?.duration_ms? result.data.item.duration_ms : 0) }
       </div>
     </div>
     <ul className="text-xs sm:text-base divide-y border-t cursor-default">
       <li className="flex items-center space-x-3 hover:bg-gray-100">
         <button className="p-3 hover:bg-green-500 group focus:outline-none">
-          <svg className="w-4 h-4 group-hover:text-white" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="5 3 19 12 5 21 5 3"></polygon></svg>
+          <svg className="w-4 h-4 group-hover:text-white" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="5 3 19 12 5 21 5 3"></polygon></svg>
         </button>
         <div className="flex-1">
           Artist - Title
@@ -99,12 +99,12 @@ function ProfileCard () {
           2:58
         </div>
         <button className="focus:outline-none pr-4 group">
-          <svg className="w-4 h-4 group-hover:text-green-600" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 15v4c0 1.1.9 2 2 2h14a2 2 0 0 0 2-2v-4M17 9l-5 5-5-5M12 12.8V2.5"/></svg>
+          <svg className="w-4 h-4 group-hover:text-green-600" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 15v4c0 1.1.9 2 2 2h14a2 2 0 0 0 2-2v-4M17 9l-5 5-5-5M12 12.8V2.5"/></svg>
         </button>
       </li>
       <li className="flex items-center space-x-3 hover:bg-gray-100">
         <button className="p-3 hover:bg-green-500 group focus:outline-none">
-          <svg className="w-4 h-4 group-hover:text-white" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="5 3 19 12 5 21 5 3"></polygon></svg>
+          <svg className="w-4 h-4 group-hover:text-white" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="5 3 19 12 5 21 5 3"></polygon></svg>
         </button>
         <div className="flex-1">
           Artist - Title
@@ -113,7 +113,7 @@ function ProfileCard () {
           2:58
         </div>
         <button className="focus:outline-none pr-4 group">
-          <svg className="w-4 h-4 group-hover:text-green-600" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 15v4c0 1.1.9 2 2 2h14a2 2 0 0 0 2-2v-4M17 9l-5 5-5-5M12 12.8V2.5"/></svg>
+          <svg className="w-4 h-4 group-hover:text-green-600" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 15v4c0 1.1.9 2 2 2h14a2 2 0 0 0 2-2v-4M17 9l-5 5-5-5M12 12.8V2.5"/></svg>
         </button>
       </li>
     </ul>
